0a1bca694ade6bc5014e39c001c250b05e9f3872
Mantas Moncevicius <moncevicius@googlemail.com>
2023-10-10 19:53:53 +0300

    feat: multiple exercises done in 8-6 Kyu

---
+++ 8 Kyu/Sum Mixed Array.js
@@ -0,0 +1,13 @@
+/**
+ * Given an array of integers as strings and numbers, return the
+ * sum of the array values as if all were numbers.
+ *
+ * Return your answer as a number.
+ *
+ * @param {array} x
+ * @returns {number}
+ */
+
+function sumMix(x){
+    return x.map(Number).reduce((sum,num) => sum + num);
+}
